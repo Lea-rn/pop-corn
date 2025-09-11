9b70008caf978d85996547da436d5dfcd7261e5b
achrefmechergui <achreffcb123achref@gamil.com>
2025-09-11 21:51:20 +0100

    useEffect

---
+++ popcorn/src/Appcopy.js
@@ -0,0 +1,245 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const average = (arr) => arr.reduce((acc, ele) => acc + ele / arr.length, 0);
+
+const KEY = "d997c473";
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState(tempWatchedData);
+
+  useEffect(function () {
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=king`);
+      const data = await res.json();
+      console.log(data.Search);
+      setMovies(data.Search);
+    }
+    fetchMovies();
+  }, []);
+  ///// array of dependencies (  [] )
+  return (
+    <div>
+      <Navbar>
+        <Logo />
+        <Search />
+        <Numresults movies={movies} />
+      </Navbar>
+
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
+        {/* <Box element={<MovieList movies={movies} />} />
+        <Box
+        elementx={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />
+            </>
+          }
+        /> */}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
+
+function Navbar({ children }) {
+  return <nav className="navbar">{children}</nav>;
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span className="logo-img">🍿</span>
+      <span className="title">usePopcorn</span>
+    </div>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      className="search"
+      type="text"
+      placeholder="serach movies ..."
+    />
+  );
+}
+
+function Numresults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
+
+function Main({ children }) {
+  return <div className="main-container">{children}</div>;
+}
+
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="box">
+      <button onClick={() => setIsOpen((cur) => !cur)} className="btn-toggle">
+        {isOpen ? "-" : "+"}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+
+function MovieList({ movies }) {
+  return (
+    <div className="movie-list-container">
+      {movies.map((ele) => (
+        <Movie movie={ele} key={ele.imdbID} />
+      ))}
+    </div>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <div className="movie-card">
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <div className="movie-card-info">
+        <h3>{movie.Title}</h3>
+        <p>
+          <span style={{ marginRight: "5px" }}>📅</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function WatchedSummary({ watched }) {
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRunTime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <div className="summary">
+      <h2>Movie you watched</h2>
+      <div className="summary-info">
+        <p>
+          <span>*️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+
+        <p>
+          <span>⭐</span>
+          <span>{avgImdbRating}</span>
+        </p>
+
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+
+        <p>
+          <span>⌛</span>
+          <span>
+            {avgRunTime} <strong>min</strong>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function WatchedMovieList({ watched }) {
+  return (
+    <div>
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </div>
+  );
+
+  function WatchedMovie({ movie }) {
+    return (
+      <div className="watched-card">
+        <img src={movie.Poster} alt={`${movie.Title}`} />
+        <div className="watch-list-container">
+          <h3>{movie.Title}</h3>
+          <div className="watched-list-info">
+            <p>
+              <span>⭐</span>
+              <span>{movie.imdbRating}</span>
+            </p>
+
+            <p>
+              <span>🌟</span>
+              <span>{movie.userRating}</span>
+            </p>
+
+            <p>
+              <span>⌛</span>
+              <span>{movie.runtime}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import StarRating from './StarRating';
+import App from './Appcopy'
 
 function TestStar(){
   const [movieRating , setMovieRating] = useState(0) ;
@@ -19,9 +21,9 @@ function TestStar(){
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
 
-<TestStar/>
+
 
        
     
